a4e031c47b796417ea5e4143cdc6a9f5bbb0c36e
Guilherme Pedrinho <pv25215@alunos.estgv.ipv.pt>
2025-02-04 15:21:35 +0000

    created Finder class

--- aStar.py
+++ aStar.py
@@ -5,115 +5,121 @@ import pygame
 from time import sleep
 import threading
 
-WIDTH = 800
-pygame.init()
-screen = pygame.display.set_mode((WIDTH,WIDTH))
-clock = pygame.time.Clock()
-screen.fill((WHITE))
-grid = Grid(40)
 
 
+class Finder:
+    def __init__(self, grid):
+        self.openSet = []# List of explorable nodes
+        self.closedSet = []# List of nodes explored allready
+        self.grid = grid
 
-running = True
-grid.initGrid()
-openSet = [] # List of explorable nodes
-closedSet = [] # List of nodes explored allready
-findingPath = True
-foundPath = False
+    def isTargetNode(self, nodeA, nodeB):
+        if nodeA.x == nodeB.x and nodeA.y == nodeB.y:
+            return True
+        return False
 
+    def getLowerCostNode(self, list):
+        current = list[0]
+        for node in list[1: ]:
+            if node.fCost() < current.fCost() or node.fCost == current.fCost and node.hCost < current.hCost:
+                current = node
+        return current
 
-def isTargetNode(nodeA, nodeB):
-    if nodeA.x == nodeB.x and nodeA.y == nodeB.y:
-        return True
-    return False
+    def reset(self):
+        self.openSet.clear()
+        self.closedSet.clear()
+        self.grid.cleanGrid()
 
-def getLowerCostNode(list):
-    current = list[0]
-    for node in list[1: ]:
-        if node.fCost() < current.fCost() or node.fCost == current.fCost and node.hCost < current.hCost:
-            current = node
-    return current
+    def findPath(self):
+        while self.openSet:
+            #print(f"OPEN SET = [{len(openSet)}]")
+            currentNode = self.getLowerCostNode(self.openSet) #Node with lowest fCost
+            # currentNode.show()
+            self.openSet.remove(currentNode)
+            self.closedSet.append(currentNode)
 
+            if self.isTargetNode(currentNode, self.grid.endNode):
+                self.grid.createPath(self.grid.startNode, self.grid.endNode)
+                print("GREAT SUCCESS")
+                return
+            
+            neighbours = self.grid.getNeighbours(currentNode)
 
-
-def findPath(findingPath, openSet, closedSet, targetNode):
-    while openSet:
-        print(f"OPEN SET = [{len(openSet)}]")
-        currentNode = getLowerCostNode(openSet) #Node with lowest fCost
-        # currentNode.show()
-        openSet.remove(currentNode)
-        closedSet.append(currentNode)
-
-        if isTargetNode(currentNode, targetNode):
-            grid.createPath(grid.startNode, targetNode)
-            print("GREAT SUCCESS")
-            return
-        
-        neighbours = grid.getNeighbours(currentNode)
-
-        for neighbour in neighbours:
-            if not neighbour.walkable or neighbour in closedSet:
-                continue
-                
-            newDstToNeighbour = currentNode.gCost +  grid.getDistance(currentNode, neighbour)
-            if newDstToNeighbour < neighbour.gCost or neighbour not in openSet:
-                neighbour.gCost = newDstToNeighbour
-                neighbour.hCost = grid.getDistance(neighbour, targetNode)
-                neighbour.parent = currentNode
-                if neighbour not in openSet:
-                    openSet.append(neighbour)
-            neighbour.color = RED
-            sleep(0.01)
-    print("PATH NOT FOUND")
-    return
+            for neighbour in neighbours:
+                if not neighbour.walkable or neighbour in self.closedSet:
+                    continue
+                    
+                newDstToNeighbour = currentNode.gCost +  self.grid.getDistance(currentNode, neighbour)
+                if newDstToNeighbour < neighbour.gCost or neighbour not in self.openSet:
+                    neighbour.gCost = newDstToNeighbour
+                    neighbour.hCost = self.grid.getDistance(neighbour, self.grid.endNode)
+                    neighbour.parent = currentNode
+                    if neighbour not in self.openSet:
+                        self.openSet.append(neighbour)
+                neighbour.color = RED
+                sleep(0.01)
+        print("PATH NOT FOUND")
+        return
             
 
+def main():
+    WIDTH = 800
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH,WIDTH))
+    clock = pygame.time.Clock()
+    screen.fill((WHITE))
+    grid = Grid(40)
+    finder = Finder(grid)
 
-while running:
-    grid.drawGrid()
-    if not foundPath:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                if event.button == 1:
-                    grid.changeCell(pos, CYAN)
-                elif event.button == 2:
-                    grid.changeCell(pos, BLACK)
-                elif event.button == 3:
-                    grid.changeCell(pos, ORANGE)
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    grid.cleanGrid()
-                    openSet.clear()
-                    closedSet.clear()
-                    print("Grid cleaned")
-                if event.key == pygame.K_a:
-                    grid.getNeighbours(grid.startNode)
-                if event.key == pygame.K_q:
-                    openSet.append(grid.startNode)
-                    # print("\nSTART NODE")
-                    # print(grid.startNode.show())
-                    # print("\nEND NODE")
-                    # print(grid.endNode.show())
-                    t1 = threading.Thread(target=findPath, args=(findingPath, openSet, closedSet, grid.endNode), daemon=True)
-                    t1.start()
-                    foundPath = True
-    else:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    grid.cleanGrid()
-                    openSet.clear()
-                    closedSet.clear()
-                    print("Grid cleaned")
-                    foundPath = False
+    running = True
+    grid.initGrid()
+    findingPath = True
+    foundPath = False
+    while running:
+        finder.grid.drawGrid()
+        if not foundPath:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    if event.button == 1:
+                        finder.grid.changeCell(pos, CYAN)
+                    elif event.button == 2:
+                        finder.grid.changeCell(pos, BLACK)
+                    elif event.button == 3:
+                        finder.grid.changeCell(pos, ORANGE)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        finder.reset()
+                        print("Grid cleaned")
+                    if event.key == pygame.K_a:
+                        finder.grid.getNeighbours(grid.startNode)
+                    if event.key == pygame.K_q:
+                        finder.openSet.append(grid.startNode)
+                        # print("\nSTART NODE")
+                        # print(grid.startNode.show())
+                        # print("\nEND NODE")
+                        # print(grid.endNode.show())
+                        t1 = threading.Thread(target=finder.findPath, daemon=True)
+                        t1.start()
+                        foundPath = True
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        finder.reset()
+                        print("Grid cleaned")
+                        foundPath = False
 
-    clock.tick(60)
+        clock.tick(60)
 
-    pygame.display.update()
+        pygame.display.update()
 
-pygame.quit()
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
